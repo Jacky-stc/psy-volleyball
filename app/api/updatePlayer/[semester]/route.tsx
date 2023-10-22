75507479ce6bcbaf0ffeceb38bc63206829b5557
Jacky Su <jacky880311@gmail.com>
2023-10-22 18:09:47 +0800

    feat: update aws-sdk from v2 to v3

--- app/api/updatePlayer/[semester]/route.tsx
+++ app/api/updatePlayer/[semester]/route.tsx
@@ -22,73 +22,49 @@ export async function GET(
     const command = new ListObjectsV2Command({
       Bucket: "psy-volleyball",
       Prefix: `player/${semester}/`,
-      MaxKeys: 1,
+      //   MaxKeys: 1,
     });
-    let isTruncated = true;
+    const { Contents } = await client.send(command);
+    const getDbData = await query({
+      query: "SELECT * FROM player",
+      values: [],
+    });
+    const dbData = JSON.parse(JSON.stringify(getDbData));
+    const s3Players = Contents?.map((player) => {
+      const playerInfo: string[] | undefined = player.Key?.slice(11, -4).split(
+        "-"
+      );
+      const semester = player.Key?.slice(7, 10);
+      if (playerInfo) {
+        return [...playerInfo, semester];
+      }
+    });
+    console.log(s3Players);
 
-    console.log("Your bucket contains the following objects:\n");
-    let contents = "";
-
-    while (isTruncated) {
-      const { Contents, IsTruncated, NextContinuationToken } =
-        await client.send(command);
-      const contentsList = Contents?.map((c) => ` • ${c.Key}`).join("\n");
-      contents += contentsList + "\n";
-      isTruncated = IsTruncated;
-      command.input.ContinuationToken = NextContinuationToken;
-    }
-    console.log(contents);
-    // AWS.config.update({
-    //   accessKeyId: process.env.accessKeyId,
-    //   secretAccessKey: process.env.secretAccessKey,
-    //   region: process.env.AWSregion,
-    // });
-    // const s3 = new S3();
-    const param = {
-      Bucket: "psy-volleyball",
-      Prefix: `player/${semester}/`,
-    };
-    // const data = await s3.listObjectsV2(param);
-    // console.log(data);
-    // const getDbData = await query({
-    //   query: "SELECT * FROM player",
-    //   values: [],
-    // });
-    // const dbData = JSON.parse(JSON.stringify(getDbData));
-    // const s3Players = data.Contents?.map((player) => {
-    //   const playerInfo: string[] | undefined = player.Key?.slice(11, -4).split(
-    //     "-"
-    //   );
-    //   const semester = player.Key?.slice(7, 10);
-    //   if (playerInfo) {
-    //     return [...playerInfo, semester];
-    //   }
-    // });
-
-    // const dbPlayerNames = dbData.map((player: Array<object>) => {
-    //   return player["name"];
-    // });
-    // const missingPlayer = s3Players?.filter(
-    //   (player) => !dbPlayerNames.includes(player[0]) && player[0] !== ""
-    // );
-    // console.log(missingPlayer);
-    // missingPlayer?.forEach((player) => {
-    //   const name = player[0];
-    //   const position = player[1];
-    //   const gender = player[2];
-    //   const semester = player[3];
-    //   const image = `https://psy-volleyball.s3.ap-northeast-1.amazonaws.com/player/${semester}/${name}-${position}-${gender}.jpg`;
-    //   if (name) {
-    //     try {
-    //       const updateDatabase = query({
-    //         query: `INSERT INTO player(name, position, semester, gender, image) VALUES(?,?,?,?,?)`,
-    //         values: [name, position, semester, gender, image],
-    //       });
-    //     } catch (error) {
-    //       console.log(error);
-    //     }
-    //   }
-    // });
+    const dbPlayerNames = dbData.map((player: Array<object>) => {
+      return player["name"];
+    });
+    const missingPlayer = s3Players?.filter(
+      (player) => !dbPlayerNames.includes(player[0]) && player[0] !== ""
+    );
+    console.log(missingPlayer);
+    missingPlayer?.forEach((player) => {
+      const name = player[0];
+      const position = player[1];
+      const gender = player[2];
+      const semester = player[3];
+      const image = `https://psy-volleyball.s3.ap-northeast-1.amazonaws.com/player/${semester}/${name}-${position}-${gender}.jpg`;
+      if (name) {
+        try {
+          const updateDatabase = query({
+            query: `INSERT INTO player(name, position, semester, gender, image) VALUES(?,?,?,?,?)`,
+            values: [name, position, semester, gender, image],
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    });
     return NextResponse.json(
       { message: "successfully update database" },
       { status: 200 }
