75d84e3305de69dd31024788f4165e20251599e7
Jacky Su <jacky880311@gmail.com>
2023-10-31 01:29:16 +0800

    feat: update port.

---
+++ app/loading.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <>
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          padding: "10rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "min(8.167vw, 240px)",
+          marginBottom: "min(12.167vw, 360px)",
+        }}
+      >
+        <h1>Loading</h1>
+        <h1>Loading</h1>
+      </div>
+    </>
+  );
+};
+
+export default loading;
